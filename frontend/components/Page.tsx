3fa62aac7e53d7e7e6bf783d088f57b7e5411a82
Sarah Holden <sarahbethholden@gmail.com>
2023-11-30 16:38:21 -0800

    Updates for page props

--- frontend/components/Page.tsx
+++ frontend/components/Page.tsx
@@ -1,13 +1,53 @@
 import { ReactNode } from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 
 type PageProps = {
   children: ReactNode;
-}
+};
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: sans-serif;
+    src: url('/static/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
+  html {
+    --black: #393939;
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: 'radnika_next', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif, --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+
+  a {
+    text-decoration: none;
+    color: var(---black);
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+`;
 
 export default function Page({children}: PageProps) {
   return (
     <div>
-      Page Wrapper
+      <GlobalStyles/>
       {children}
     </div>
   )
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,9 @@
+import { ReactElement } from "react";
 import Page from "../components/Page";
+import type { AppProps, AppContext } from 'next/app';
 
 
-export default function TravelApp({Component, pageProps}: any) {
+export default function TravelApp({Component, pageProps}: AppProps) {
   return (
     <Page>
       <Component {...pageProps} />
