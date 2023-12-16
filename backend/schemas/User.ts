1bdf794e026d4c0d0308bf096cfbf67846eeb175
Sarah Holden <sarahbethholden@gmail.com>
2023-12-15 16:29:13 -0800

    Working through permissions on an access level (backend)

--- backend/schemas/User.ts
+++ backend/schemas/User.ts
@@ -1,7 +1,18 @@
 import { list } from '@keystone-next/keystone/schema';
 import { text, password, relationship } from '@keystone-next/fields';
+import { permissions, rules } from '../access';
 
 export const User = list({
+  access: {
+    create: () => true,
+    read: rules.canManageUsers,
+    update: rules.canManageUsers,
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    hideCreate: (args) => !permissions.canManageUsers(args),
+    hideDelete: (args) => !permissions.canManageUsers(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     email: text({ isRequired: true, isUnique: true }),
@@ -17,7 +28,10 @@ export const User = list({
     }),
     role: relationship({
       ref: 'Role.assignedTo',
-      // TODO: Add access control
+      access: {
+        create: permissions.canManageUsers,
+        update: permissions.canManageUsers,
+      },
     }),
     guides: relationship({
       ref: 'Guide.user',
