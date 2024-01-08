c0ac5ad2c765c87f12baf28000b92d3548d208db
Sarah Holden <sarahbethholden@gmail.com>
2024-01-08 14:30:55 -0800

    Updates for testing

--- frontend/__tests__/Nav.test.js
+++ frontend/__tests__/Nav.test.js
@@ -25,8 +25,25 @@ describe('<Nav/>', () => {
           <Nav/>
         </MockedProvider>
     )
-    expect(container).toHaveTextContent(/guides/i)
+    expect(container).toMatchSnapshot()
+    expect(screen.getByText(/sign in/i)).toHaveAttribute('href', '/signin')
     expect(container).toHaveTextContent(/sign in/i)
-    debug()
+    expect(container).toHaveTextContent(/guides/i)
+    expect(screen.getByText(/guides/i)).toHaveAttribute("href", "/guides");
+  })
+});
+
+describe('<Nav/>', () => {
+  it('Renders a minimal nav when signed out', async () => {
+    const { container, debug } = render(
+        <MockedProvider mocks={signedInMocks}>
+          <Nav/>
+        </MockedProvider>
+    )
+    expect(container).toMatchSnapshot()
+    expect(container).toHaveTextContent(/guides/i);
+    expect(screen.getByText(/guides/i)).toHaveAttribute("href", "/guides");
+    await screen.findAllByText('Account')
+    expect(screen.getByText(/account/i)).toHaveAttribute("href", "/account");
   })
 })
